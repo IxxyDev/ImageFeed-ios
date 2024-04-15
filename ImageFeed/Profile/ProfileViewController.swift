14a40898cd55d06f29884730caaa6114e46c78a1
IxxyDev <denisenkoforwork@gmail.com>
2024-04-15 23:35:36 +0300

    feat: base markup ready

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UIView!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    
+    @IBOutlet var logoutButton: UIButton!
+}
