0de18e60899b24c8f213f6d34dc5017af53567cf
IxxyDev <denisenkoforwork@gmail.com>
2024-05-11 15:52:57 +0300

    feat: add unisplash constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "585WluroZ2wFL3eZog0vRYSPMP2HCnlQoDTMn5pUJQI"
+    static let secureKey = "GNa-U0wHtn0K0qJolgf_yHqsTcDPWKPEf9tzXfP_GOs"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "pubilic+read_user+write_likes"
+}
